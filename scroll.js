f95be13079c49929b436db73d5e3015392e6900e
Rafiza <becktothelife@gmail.com>
2022-07-06 02:08:43 -0700

    first commit

---
+++ scroll.js
@@ -0,0 +1,10 @@
+import 'https://flackr.github.io/scroll-timeline/dist/scroll-timeline.js';
+
+// function Person(name, age, year){
+//   this.name = name;
+//   this.age = age;
+//   this.year = year;
+// }
+// const person1 = new Person('rafiza', 23, '2019')
+// console.log(person1)
+ScrollTimeline
